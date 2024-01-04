217134047f4964fd5fe91bb3b31a60827a1a4e04
monteen <montwork01@gmail.com>
2024-01-04 15:46:02 +0530

    changes the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  const previousYear = currentYear - 1;
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
+        &copy;{previousYear}-{currentYear} Monteen™️. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
